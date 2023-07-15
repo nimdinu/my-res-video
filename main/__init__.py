3bca759180d7db280d9c2400b418c6122a46fc6d
nimdinu <69969943+nimdinu@users.noreply.github.com>
2023-07-15 22:09:24 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = 1735265
+API_HASH = '3d35e17cfbbe90c1bc69baf97cd52016'
+BOT_TOKEN = '6329273158:AAH6OVMPuo4Xtvz1jFTmNf6tOhqDqeR5Zbs'
+SESSION = 'BQAaemEAjWsifkL8X-y36O_ospvAXoAbSQlJFMrc5ZpwcOwLj18bs_cpSNwQehNDzXAwI_0_ZA5INwTFn4IcEy92LvlIN9GFbDyQcJ7CWTtm32EclxUQCNpUON6YZrWffxq5oZoaTjoNPqX2gyi77MLvGsVuyAQR5jEmq8zBKzhO6aE2D8fQNUNmUvl9STFYANWya8Yx2O2F-0Q2xF190OSVWMrBR6IDtsPZ4IQuKRZyObpYHO5vCsJ5E6kS4KEkfeXRjRvndRYOvgROQx2qusQWftU3R2dBL650Z3LkmEbAG48gZ4MNuhEpD1AR4yBtN3EiJU1XzWAA-PulF9QHbyRPrFP2qAAAAABCl0-TAA'
+FORCESUB = 'nimbackup'
+AUTH = 1117212563
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
